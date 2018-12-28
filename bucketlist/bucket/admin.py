e267d2b5da6d67fbba68b04d2b16f357df5756d4
junngo <myeongjun.ko@gmail.com>
2018-12-28 21:44:26 +0900

    Creating models and Registering the models in the admin

--- bucketlist/bucket/admin.py
+++ bucketlist/bucket/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Bucket)
+class BucketAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        'created_at',
+        'updated_at',
+        'completed_at',
+        'my_wish',
+        'creator',
+        'memo',
+    )
--- bucketlist/bucket/models.py
+++ bucketlist/bucket/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from bucketlist.users import models as user_models
 
 # Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    completed_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Bucket(TimeStampedModel):
+
+    """ Bucket Model """
+
+    my_wish = models.CharField(max_length=140)
+    memo = models.TextField(null=True, blank=True)
+    creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE, related_name='buckets')
+
+    def __str__(self):
+        return self.my_wish
