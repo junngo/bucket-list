631091724b4812e89947dee8603985aa214deec8
junngo <myeongjun.ko@gmail.com>
2019-01-15 21:38:37 +0900

    input and creating bucket

--- bucketlist/bucket/views.py
+++ bucketlist/bucket/views.py
@@ -1,20 +1,37 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from .models import Bucket
+from . import models
 
 # Create your views here.
 def index(request):
 
-    latest_bucket_list = Bucket.objects.order_by('-created_at')[:5]
-    #template = loader.get_template('bucket/index.html')
+    if request.method == 'POST':
+        """ POST """
+        input_bucket = request.POST['input_bucket']
+        user = request.user
 
-    context = {
-        'bucket_list': latest_bucket_list,
-    }
+        new_bucket = models.Bucket.objects.create(
+            my_wish = input_bucket,
+            creator = user
+        )
+        new_bucket.save
 
-    #return HttpResponse(template.render(context, request))
-    return render(request, 'bucket/index.html', context)
+        return redirect('/bucket/')
+
+    else:
+        """ GET """
+
+        latest_bucket_list = Bucket.objects.order_by('-created_at')[:5]
+        #template = loader.get_template('bucket/index.html')
+
+        context = {
+            'bucket_list': latest_bucket_list,
+        }
+
+        #return HttpResponse(template.render(context, request))
+        return render(request, 'bucket/index.html', context)
 
 
 def detail(request, bucket_id):
